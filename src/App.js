5ee1bcc062b148820a19edf70f07fa22491ba206
Jamgoori <lks2354@naver.com>
2023-01-14 16:43:56 +0900

    라우터 기본설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import {BrowserRouter, Route} from "react-router-dom";
 import './App.css';
+import Header from "./components/Header";
+import CoinPage from "./Pages/CoinPage";
+import HomePage from "./Pages/HomePage";
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+    <BrowserRouter>
+  <Header/>
+  <Route path="/" component={HomePage} exact/>
+  <Route path="/coins/:id" component={CoinPage}/>
+  </BrowserRouter>
+    </>
+
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Headsssser</div>
+  )
+}
+
+export default Header
